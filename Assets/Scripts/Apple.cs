592ba3e67d4241e6e569fc5ea414074e6c1bfa0c
sfsavard <45376083+sfsavard@users.noreply.github.com>
2019-09-26 22:04:19 -0400

    updating high score

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -11,7 +11,13 @@ public class Apple : MonoBehaviour
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);                                      // b
+
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();   // b
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();                                      // c
         }
     }
 }
+        
 
--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,10 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;     // This line enables use of uGUI features.        // a
 
 public class Basket : MonoBehaviour
 {
-    void Update()
+    [Header("Set Dynamically")]
+    public Text scoreGT;                                          // a
+
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");              // b
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();                            // c
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
+  
+
+    
+
+
+void Update()
     {
         // Get the current screen position of the mouse from Input
         Vector3 mousePos2D = Input.mousePosition;                             // a
@@ -21,16 +40,29 @@ public class Basket : MonoBehaviour
         this.transform.position = pos;
     }
 
-        void OnCollisionEnter(Collision coll)
-        {                         // a
-                                  // Find out what hit this basket
-            GameObject collidedWith = coll.gameObject;                    // b
-            if (collidedWith.tag == "Apple")
-            {                          // c
-                Destroy(collidedWith);
+    void OnCollisionEnter(Collision coll)
+    {
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+
+            // Parse the text of the scoreGT into an int
+            int score = int.Parse(scoreGT.text);                         // d
+            // Add points for catching the apple
+            score += 100;
+            // Convert the score back to a string and display it
+            scoreGT.text = score.ToString();
+
+            // Track the high score
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
             }
         }
- }
+    }
+}
 
 
 
